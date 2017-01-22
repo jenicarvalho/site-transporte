18c693c8e6a3ce863a9cb95826a9b85e87e81149
Jeniffer Carvalho <jenicarvalho@live.com>
2017-01-22 20:17:27 -0200

    init()

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp 	     = require('gulp');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var plumber      = require('gulp-plumber');
+var nextcss 	 = require('postcss-cssnext');
+var nested		 = require('postcss-nested');
+var importer	 = require('postcss-import');
+var browserSync = require('browser-sync').create();
+var fileinclude = require('gulp-file-include');
+var imagemin    = require('gulp-imagemin');
+var changed     = require('gulp-changed');
+
+gulp.task('css', function () {
+    return gulp.src('src/pre-css/style.css')
+        .pipe( plumber() )
+        .pipe( sourcemaps.init() )
+        .pipe( postcss([importer, nested, nextcss]) )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest('dist/css/') );
+});
+
+gulp.task('watch', function() {
+	return gulp.watch('src/pre-css/*.css', ['css']);
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    notify: true,
+    port: 9003,
+    server: './dist'
+  });
+});
+
+gulp.task('include',  function() {
+  return gulp.src(['./src/**/*.html', '!./src/inc/**/*'])
+      .pipe(plumber())
+      .pipe(fileinclude({
+        prefix: '@@',
+        basepath: 'src/'
+      }))
+      .pipe(gulp.dest('./dist'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+  return gulp.src('./src/img/**/*')
+      .pipe(plumber())
+      .pipe(changed('./dist/img'))
+      .pipe(imagemin())
+      .pipe(gulp.dest('./dist/img'))
+      .pipe(browserSync.stream());
+});
+
+
+gulp.task('build', ['css', 'watch', 'serve', 'include', 'images']);
+gulp.task('default', ['css', 'build']);
