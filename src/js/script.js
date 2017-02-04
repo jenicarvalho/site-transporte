ef584f8ba20e1d856a590e69dc107a4f5e91c739
Jeniffer Carvalho <jenicarvalho@live.com>
2017-02-04 00:06:52 -0200

    script de scroll

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,12 @@ $('.bxslider').bxSlider({
   mode: 'fade',
   captions: true
 });
+
+function scroll(){
+	$('a[href*=#]').on('click', function(event){     
+	    event.preventDefault();
+	    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+}
+
+scroll();
